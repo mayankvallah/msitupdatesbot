160b40145684d17793a8ecf9cc45cceb5a294a60
Kashyyk <mayankvallah@laptop.com>
2021-02-25 09:00:56 +0530

    Added notice section

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import schedule
 
 
 
-def crawling():
+def crawlingNews():
     #'''
     #Args: website_link = string; link of website to be crawled
           #link_class = string; class name for job link on website
@@ -30,6 +30,23 @@ def crawling():
     
     
 
+def crawlingNotice():
+    #'''
+    #Args: website_link = string; link of website to be crawled
+          #link_class = string; class name for job link on website
+    #Returns: jobs_link = list; list of jobs 
+    #'''
+    
+    website_request = requests.get('http://msit.in/notices')
+    website_content = BeautifulSoup(website_request.content, 'html.parser')
+
+    #extract job description
+    notices_link = website_content.select('div.tab-content a')
+    return notices_link
+    #links = []
+    #titles = []
+    #x = 0
+
 
 def get_url(url):
     response = requests.get(url)
@@ -40,7 +57,7 @@ def get_url(url):
 def send_message(chat_id, text, doc):
     TOKEN = "1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c"
     URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-    url = URL + f"sendDocument?chat_id=-1001454545667&caption=\"{text}\"&document={doc}"
+    url = URL + f"sendDocument?chat_id=-1001454545667&caption={text}&document={doc}"
     print(url)
     get_url(url)
     
@@ -58,7 +75,8 @@ def check_result_send_mess():
     except:
        send_message(chat_id, 'The database could not be accessed')
 
-    jobs_link_pm = crawling()
+    jobs_link_pm = crawlingNews()
+    notices_link = crawlingNotice()
     
     for item in reversed(jobs_link_pm):
         job_exists = jobs_db.execute('SELECT job FROM jobs WHERE job = %s', [item.text])
@@ -73,6 +91,19 @@ def check_result_send_mess():
         else:
             continue
         
+    for item in reversed(notices_link):
+        job_exists = jobs_db.execute('SELECT job FROM jobs WHERE job = %s', [item.text])
+        
+        if len(jobs_db.fetchall()) != 1:
+            mess_content = f"LATEST NOTICE: {item.text}"
+            doc = f"http://msit.in{item['href']}"
+            print(doc)
+            send_message("-1001454545667", mess_content, doc)
+            jobs_db.execute('INSERT INTO jobs (job) VALUES (%s);', [item.text])
+            conn.commit()
+        else:
+            continue
+
     jobs_db.close()
     
     
@@ -86,8 +117,6 @@ bot = "1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c"
 chat_id = "-1001454545667"
 check_result_send_mess()
 #schedule crawler
-#schedule.every().day.at("08:00").do(check_result_send_mess)
-
-#run script infinitely
-#while True:
-    #schedule.run_pending()
+schedule.every().hour.do(check_result_send_mess)
+while True:
+    schedule.run_pending()
