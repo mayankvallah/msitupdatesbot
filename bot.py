dcf276f91e252c8a16433602b16ba693c9e5fd0f
mayankvallah <62397838+mayankvallah@users.noreply.github.com>
2021-02-25 17:52:13 +0530

    Updated security

--- bot.py
+++ bot.py
@@ -5,85 +5,48 @@ import psycopg2
 import sqlite3 as lite
 import schedule
 
-
+chat_id = ""
+TOKEN = ""
 
 def crawlingNews():
-    #'''
-    #Args: website_link = string; link of website to be crawled
-          #link_class = string; class name for job link on website
-    #Returns: jobs_link = list; list of jobs 
-    #'''
     
     website_request = requests.get('http://msit.in/latest_news')
     website_content = BeautifulSoup(website_request.content, 'html.parser')
-
-    #extract job description
     jobs_link = website_content.select('div.tab-content a')
     return jobs_link
-    #links = []
-    #titles = []
-    #x = 0
-
-    #for anchors in main_div:
-        #titles.append(anchors.text)
-        #links.append(f"http://msit.in/media/news{anchors['href']}\n\n")
-    
-    
 
 def crawlingNotice():
-    #'''
-    #Args: website_link = string; link of website to be crawled
-          #link_class = string; class name for job link on website
-    #Returns: jobs_link = list; list of jobs 
-    #'''
     
     website_request = requests.get('http://msit.in/notices')
     website_content = BeautifulSoup(website_request.content, 'html.parser')
 
-    #extract job description
     notices_link = website_content.select('div.tab-content a')
     return notices_link
-    #links = []
-    #titles = []
-    #x = 0
 
 def crawlingMarq():
-    #'''
-    #Args: website_link = string; link of website to be crawled
-          #link_class = string; class name for job link on website
-    #Returns: jobs_link = list; list of jobs 
-    #'''
     
     website_request = requests.get('http://msit.in/')
     website_content = BeautifulSoup(website_request.content, 'html.parser')
 
-    #extract job description
     marq_link = website_content.select('div.marquee-text a')
     return marq_link
-    #links = []
-    #titles = []
-    #x = 0
     
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
-
 def send_message(chat_id, text, doc):
-    TOKEN = "1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c"
+    TOKEN = ""
     URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-    url = URL + f"sendDocument?chat_id=-1001454545667&parse_mode=html&caption={text}&document={doc}"
+    url = URL + f"sendDocument?chat_id={chat_id}&parse_mode=html&caption={text}&document={doc}"
     print(url)
     get_url(url)
-    
-    #parameters = {'chat_id': chat_id, 'text': text}
-    #message = requests.post("1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c" + 'sendMessage', data=parameters)
 
 def send_marq(chat_id, text, doc):
-    TOKEN = "1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c"
+    TOKEN = ""
     URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-    url = URL + f"sendMessage?chat_id=-1001454545667&parse_mode=html&text={text} \n {doc}"
+    url = URL + f"sendMessage?chat_id={chat_id}&parse_mode=html&text={text}<br>{doc}"
     print(url)
     get_url(url)
 
@@ -107,7 +70,7 @@ def check_result_send_mess():
         if len(jobs_db.fetchall()) != 1:
             news_content = f"<b>LATEST NEWS</b>: {item.text}"
             news_doc = f"http://msit.in{item['href']}"
-            send_message("-1001454545667", news_content, news_doc)
+            send_message(chat_id, news_content, news_doc)
             jobs_db.execute('INSERT INTO jobs (job) VALUES (%s);', [item.text])
             conn.commit()
         else:
@@ -119,7 +82,7 @@ def check_result_send_mess():
         if len(jobs_db.fetchall()) != 1:            
             notices_content = f"<b>LATEST NOTICE</b>: {item.text}"
             notices_doc = f"http://msit.in{item['href']}"
-            send_message("-1001454545667", notices_content, notices_doc)
+            send_message(chat_id, notices_content, notices_doc)
             jobs_db.execute('INSERT INTO jobs (job) VALUES (%s);', [item.text])
             conn.commit()
         else:
@@ -137,19 +100,14 @@ def check_result_send_mess():
             else:
                 doc = f"{item['href']}"
             print(doc)
-            send_marq("-1001454545667", mess_content, doc)
+            send_marq(chat_id, mess_content, doc)
             jobs_db.execute('INSERT INTO jobs (job) VALUES (%s);', [item.text])
             conn.commit()
         else:
             continue
     
     jobs_db.close()
-    
-    
 
-# bot and chat ids
-bot = "1638409055:AAHgC5KE51zFqpLZbw5V5qXRfzdKeczlL-c"
-chat_id = "-1001454545667"
 #check_result_send_mess()
 schedule.every().hour.do(check_result_send_mess)
 while True:
